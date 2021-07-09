da4aa72790fabbc29cded3b19a0bc3b226d59d7b
LynAudubon <lyn_audubon07@yahoo.com>
2021-07-08 21:28:45 -0700

    day1 progress

---
+++ main.js
@@ -0,0 +1,13 @@
+const content = document.getElementById('content');
+const contentParent = content.parentNode;
+
+console.log("contentParents",contentParent)
+content.parentNode.removeChild(content);
+
+const header = document.createElement('HEADER');
+header.innerHTML = 'Get back to achieving your coding dreams';
+contentParent.appendChild(header);
+
+const image = document.createElement('img');
+image.src = 'https://images.unsplash.com/photo-1533749047139-189de3cf06d3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGltZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60';
+contentParent.appendChild(image);
